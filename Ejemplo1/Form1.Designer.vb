56c9c5ecb0915b2f07d20a21f2ef3e98b8d3a241
Daniela Paula Bongiorno <78030777+DanielaPaulaBongiorno@users.noreply.github.com>
2022-04-07 11:18:56 -0300

    Add files via upload

---
+++ Ejemplo1/Form1.Designer.vb
@@ -0,0 +1,70 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class UserForm5
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.CommandButton1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'CommandButton1
+        '
+        Me.CommandButton1.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.CommandButton1.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.CommandButton1.Location = New System.Drawing.Point(374, 108)
+        Me.CommandButton1.Name = "CommandButton1"
+        Me.CommandButton1.Size = New System.Drawing.Size(96, 55)
+        Me.CommandButton1.TabIndex = 0
+        Me.CommandButton1.Text = "Pulse +"
+        Me.CommandButton1.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label1.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(306, 126)
+        Me.Label1.MaximumSize = New System.Drawing.Size(35, 35)
+        Me.Label1.MinimumSize = New System.Drawing.Size(25, 25)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(25, 25)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "0"
+        '
+        'UserForm5
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.CommandButton1)
+        Me.Name = "UserForm5"
+        Me.Text = "Formulario Ej1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents CommandButton1 As Button
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Ejemplo1/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class UserForm5
+    Private Sub CommandButton1_Click(sender As Object, e As EventArgs) Handles CommandButton1.Click
+        Dim a As Integer
+
+        a = Label1.Text
+        If (a >= 0) Then
+            a = a + 1
+            Label1.Text = a
+        End If
+    End Sub
+End Class
