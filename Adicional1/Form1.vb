92c24f4d8102bc6bb255d910dc2644428a0d1ca4
Daniela Paula Bongiorno <78030777+DanielaPaulaBongiorno@users.noreply.github.com>
2022-04-07 12:53:41 -0300

    Add files via upload

---
+++ Adicional1/Form1.vb
@@ -0,0 +1,33 @@
+﻿Public Class Form1
+
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim texto As String
+        Dim i, suma As Integer
+        Dim tamanio As Integer
+        texto = TextBox1.Text
+        tamanio = Len(texto)
+        suma = 0
+
+        For i = 1 To tamanio
+            If Mid(texto, i, 1) = "a" Then
+                suma = suma + 1
+            End If
+            If Mid(texto, i, 1) = "e" Then
+                suma = suma + 1
+            End If
+            If Mid(texto, i, 1) = "i" Then
+                suma = suma + 1
+            End If
+            If Mid(texto, i, 1) = "o" Then
+                suma = suma + 1
+            End If
+            If Mid(texto, i, 1) = "u" Then
+                suma = suma + 1
+            End If
+        Next
+
+        TextBox2.Text = suma
+
+    End Sub
+End Class
