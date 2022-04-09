f2b16717652cb7f139aadfb8d80ad3655c94a949
Daniela Paula Bongiorno <78030777+DanielaPaulaBongiorno@users.noreply.github.com>
2022-04-08 21:47:55 -0300

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,353 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button0 = New System.Windows.Forms.Button()
+        Me.ButtonIgual = New System.Windows.Forms.Button()
+        Me.ButtonSuma = New System.Windows.Forms.Button()
+        Me.ButtonResta = New System.Windows.Forms.Button()
+        Me.ButtonMulti = New System.Windows.Forms.Button()
+        Me.ButtonDivi = New System.Windows.Forms.Button()
+        Me.ButtonLimpiartodo = New System.Windows.Forms.Button()
+        Me.ButtonRaiz = New System.Windows.Forms.Button()
+        Me.ButtonCuadrado = New System.Windows.Forms.Button()
+        Me.ButtonMemo = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.Button13 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(76, 188)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(64, 57)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(146, 188)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(64, 57)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(216, 188)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(65, 57)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(75, 125)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(65, 57)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "4"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(146, 125)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(64, 57)
+        Me.Button5.TabIndex = 4
+        Me.Button5.Text = "5"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(216, 125)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(64, 57)
+        Me.Button6.TabIndex = 5
+        Me.Button6.Text = "6"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.FlatAppearance.BorderSize = 2
+        Me.Button7.Location = New System.Drawing.Point(75, 63)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(65, 56)
+        Me.Button7.TabIndex = 6
+        Me.Button7.Text = "7"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(146, 63)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(64, 56)
+        Me.Button8.TabIndex = 7
+        Me.Button8.Text = "8"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(216, 63)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(64, 56)
+        Me.Button9.TabIndex = 9
+        Me.Button9.Text = "9"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button0
+        '
+        Me.Button0.Location = New System.Drawing.Point(75, 251)
+        Me.Button0.Name = "Button0"
+        Me.Button0.Size = New System.Drawing.Size(65, 59)
+        Me.Button0.TabIndex = 10
+        Me.Button0.Text = "0"
+        Me.Button0.UseVisualStyleBackColor = True
+        '
+        'ButtonIgual
+        '
+        Me.ButtonIgual.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.ButtonIgual.Location = New System.Drawing.Point(77, 316)
+        Me.ButtonIgual.Name = "ButtonIgual"
+        Me.ButtonIgual.Size = New System.Drawing.Size(133, 59)
+        Me.ButtonIgual.TabIndex = 11
+        Me.ButtonIgual.TabStop = False
+        Me.ButtonIgual.Text = "="
+        Me.ButtonIgual.UseVisualStyleBackColor = True
+        '
+        'ButtonSuma
+        '
+        Me.ButtonSuma.Font = New System.Drawing.Font("Segoe UI", 15.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.ButtonSuma.Location = New System.Drawing.Point(287, 63)
+        Me.ButtonSuma.Name = "ButtonSuma"
+        Me.ButtonSuma.Size = New System.Drawing.Size(64, 56)
+        Me.ButtonSuma.TabIndex = 12
+        Me.ButtonSuma.Tag = "Dim memo As DoubleDim memo As Double"
+        Me.ButtonSuma.Text = "+"
+        Me.ButtonSuma.UseVisualStyleBackColor = True
+        '
+        'ButtonResta
+        '
+        Me.ButtonResta.Font = New System.Drawing.Font("Segoe UI", 15.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.ButtonResta.Location = New System.Drawing.Point(287, 125)
+        Me.ButtonResta.Name = "ButtonResta"
+        Me.ButtonResta.Size = New System.Drawing.Size(64, 56)
+        Me.ButtonResta.TabIndex = 13
+        Me.ButtonResta.Tag = "Dim memo As DoubleDim memo As Double"
+        Me.ButtonResta.Text = "-"
+        Me.ButtonResta.UseVisualStyleBackColor = True
+        '
+        'ButtonMulti
+        '
+        Me.ButtonMulti.Font = New System.Drawing.Font("Segoe UI", 15.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.ButtonMulti.Location = New System.Drawing.Point(287, 188)
+        Me.ButtonMulti.Name = "ButtonMulti"
+        Me.ButtonMulti.Size = New System.Drawing.Size(64, 56)
+        Me.ButtonMulti.TabIndex = 14
+        Me.ButtonMulti.Tag = "Dim memo As DoubleDim memo As Double"
+        Me.ButtonMulti.Text = "*"
+        Me.ButtonMulti.UseVisualStyleBackColor = True
+        '
+        'ButtonDivi
+        '
+        Me.ButtonDivi.Font = New System.Drawing.Font("Segoe UI", 15.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.ButtonDivi.Location = New System.Drawing.Point(287, 251)
+        Me.ButtonDivi.Name = "ButtonDivi"
+        Me.ButtonDivi.Size = New System.Drawing.Size(64, 59)
+        Me.ButtonDivi.TabIndex = 15
+        Me.ButtonDivi.Tag = "Dim memo As DoubleDim memo As Double"
+        Me.ButtonDivi.Text = "/"
+        Me.ButtonDivi.UseVisualStyleBackColor = True
+        '
+        'ButtonLimpiartodo
+        '
+        Me.ButtonLimpiartodo.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ButtonLimpiartodo.Font = New System.Drawing.Font("Segoe UI", 15.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.ButtonLimpiartodo.Location = New System.Drawing.Point(287, 316)
+        Me.ButtonLimpiartodo.Name = "ButtonLimpiartodo"
+        Me.ButtonLimpiartodo.Size = New System.Drawing.Size(64, 59)
+        Me.ButtonLimpiartodo.TabIndex = 16
+        Me.ButtonLimpiartodo.Tag = "Dim memo As DoubleDim memo As Double"
+        Me.ButtonLimpiartodo.Text = "C"
+        Me.ButtonLimpiartodo.UseVisualStyleBackColor = False
+        '
+        'ButtonRaiz
+        '
+        Me.ButtonRaiz.Location = New System.Drawing.Point(145, 251)
+        Me.ButtonRaiz.Name = "ButtonRaiz"
+        Me.ButtonRaiz.Size = New System.Drawing.Size(65, 59)
+        Me.ButtonRaiz.TabIndex = 18
+        Me.ButtonRaiz.Text = "Raiz"
+        Me.ButtonRaiz.UseVisualStyleBackColor = True
+        '
+        'ButtonCuadrado
+        '
+        Me.ButtonCuadrado.Location = New System.Drawing.Point(215, 251)
+        Me.ButtonCuadrado.Name = "ButtonCuadrado"
+        Me.ButtonCuadrado.Size = New System.Drawing.Size(65, 59)
+        Me.ButtonCuadrado.TabIndex = 19
+        Me.ButtonCuadrado.Text = "Pot"
+        Me.ButtonCuadrado.UseVisualStyleBackColor = True
+        '
+        'ButtonMemo
+        '
+        Me.ButtonMemo.Location = New System.Drawing.Point(77, 380)
+        Me.ButtonMemo.Name = "ButtonMemo"
+        Me.ButtonMemo.Size = New System.Drawing.Size(63, 61)
+        Me.ButtonMemo.TabIndex = 20
+        Me.ButtonMemo.Text = "MS"
+        Me.ButtonMemo.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox1.Font = New System.Drawing.Font("Segoe UI", 26.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.TextBox1.ForeColor = System.Drawing.SystemColors.MenuHighlight
+        Me.TextBox1.Location = New System.Drawing.Point(75, 6)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(276, 54)
+        Me.TextBox1.TabIndex = 21
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(145, 381)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(65, 60)
+        Me.Button10.TabIndex = 22
+        Me.Button10.Text = "MR"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Button11
+        '
+        Me.Button11.Font = New System.Drawing.Font("Segoe UI", 15.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button11.Location = New System.Drawing.Point(217, 318)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(64, 56)
+        Me.Button11.TabIndex = 23
+        Me.Button11.Tag = "Dim memo As DoubleDim memo As Double"
+        Me.Button11.Text = "."
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'Button12
+        '
+        Me.Button12.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Button12.Font = New System.Drawing.Font("Segoe UI", 15.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button12.Location = New System.Drawing.Point(287, 382)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(64, 59)
+        Me.Button12.TabIndex = 24
+        Me.Button12.Tag = "Dim memo As DoubleDim memo As Double"
+        Me.Button12.Text = "ON"
+        Me.Button12.UseVisualStyleBackColor = False
+        '
+        'Button13
+        '
+        Me.Button13.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Button13.Font = New System.Drawing.Font("Segoe UI", 15.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button13.Location = New System.Drawing.Point(216, 382)
+        Me.Button13.Name = "Button13"
+        Me.Button13.Size = New System.Drawing.Size(64, 59)
+        Me.Button13.TabIndex = 25
+        Me.Button13.Tag = "Dim memo As DoubleDim memo As Double"
+        Me.Button13.Text = "OFF"
+        Me.Button13.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.ClientSize = New System.Drawing.Size(429, 450)
+        Me.Controls.Add(Me.Button13)
+        Me.Controls.Add(Me.Button12)
+        Me.Controls.Add(Me.Button11)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.ButtonMemo)
+        Me.Controls.Add(Me.ButtonCuadrado)
+        Me.Controls.Add(Me.ButtonRaiz)
+        Me.Controls.Add(Me.ButtonLimpiartodo)
+        Me.Controls.Add(Me.ButtonDivi)
+        Me.Controls.Add(Me.ButtonMulti)
+        Me.Controls.Add(Me.ButtonResta)
+        Me.Controls.Add(Me.ButtonSuma)
+        Me.Controls.Add(Me.ButtonIgual)
+        Me.Controls.Add(Me.Button0)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Name = "Form1"
+        Me.Text = "Calculadora"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button0 As Button
+    Friend WithEvents ButtonIgual As Button
+    Friend WithEvents ButtonSuma As Button
+    Friend WithEvents ButtonResta As Button
+    Friend WithEvents ButtonMulti As Button
+    Friend WithEvents ButtonDivi As Button
+    Friend WithEvents ButtonLimpiartodo As Button
+    Friend WithEvents ButtonRaiz As Button
+    Friend WithEvents ButtonCuadrado As Button
+    Friend WithEvents ButtonMemo As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button13 As Button
+End Class
